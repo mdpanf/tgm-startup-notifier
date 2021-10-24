0d872ae5d5873a7454fc3d5bc9cf69cf3b14eecb
mdpanf <me@mdpanf.ru>
2021-10-24 13:28:57 +0300

    Fix config file

--- startup-notifier.sh
+++ startup-notifier.sh
@@ -8,15 +8,14 @@ send_msg(){
   myName=$(hostname)
   myOS=$(hostnamectl | grep System | cut -d':' -f2)
   myKernel=$(hostnamectl | grep Kernel | awk '{ print $2,$3 }')
+  myArch=$(hostnamectl | grep Architecture | awk '{ print $2 }')
   nowTime=$(date | awk '{ print $5,$6 }')
-  myAddress=$(nslookup localhost | awk NR\ ==\ 2\{print\ \$2\})
-  inetAddr=$(ifconfig | awk '{ if ($1 == "inet" ) print $2 }' | tr -s '\r\n' ' ')
+  inetAddr=$(ip a | awk '{ if ($1 == "inet" ) print $2 }' | tr -s '\r\n' ' ')
   publicIp=$(wget -t 1 -qO- ipinfo.io/ip)
 
   msg="🖥 $myName server is running <b>[up]</b>$NL"
-  msg="$msg<b>OS:</b> $myOS/$myKernel$NL"
+  msg="$msg<b>OS:</b> $myOS/$myKernel $myArch$NL"
   msg="$msg<b>Server time:</b> $nowTime$NL"
-  msg="$msg<b>Server address:</b> $myAddress$NL"
   msg="$msg<b>Inet addr:</b> $inetAddr$NL"
   msg="$msg<b>Public IP:</b> $publicIp$NL"
 
@@ -25,8 +24,8 @@ send_msg(){
   /usr/bin/curl -s -X POST $URL -d chat_id=${TGMN_CHAT_ID} -d text=${MESSAGE} -d parse_mode=${PARSE_MODE} > /dev/null 2>&1
 }
 
-TGMN_TOKEN=$(cat /etc/tgm-notifier.conf | grep Token | cut -d' ' -f2)
-TGMN_CHAT_ID=$(cat /etc/tgm-notifier.conf | grep Chat | cut -d' ' -f2)
+TGMN_TOKEN=$(cat ./sn.conf | grep Token | cut -d' ' -f2)
+TGMN_CHAT_ID=$(cat ./sn.conf | grep Chat | cut -d' ' -f2)
 
 if [ -z ${TGMN_TOKEN} ]; then
   echo "No token or chat_id"

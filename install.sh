600f8bdf956b705b6002ced896a4e5062221decf
mdpanf <me@mdpanf.ru>
2021-10-24 17:27:07 +0300

    Fix tokens 6

--- install.sh
+++ install.sh
@@ -13,9 +13,9 @@ input_data() {
 }
 
 function confirm() {
-  echo "Token: \033[0;35m$inToken\033[0m"
-  echo "Chat ID: \033[0;35m$inChatId\033[0m"
-  read -p "${1:-Is it correct? [y/N]} "
+  echo -e "Token: \033[0;35m$inToken\033[0m"
+  echo -e "Chat ID: \033[0;35m$inChatId\033[0m"
+  read -e -p "${1:-\033[0;33mIs it correct?\033[0m [y/N]} "
   case $(echo $REPLY | tr '[A-Z]' '[a-z]') in
       y|yes) echo "Saving..." ;;
       *)     input_data ;;

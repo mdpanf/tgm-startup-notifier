577be3d389a1e2d981ebef7d353ecefa7abe0506
mdpanf <me@mdpanf.ru>
2021-10-24 16:25:07 +0300

    Fix install.sh

--- install.sh
+++ install.sh
@@ -1,52 +1,29 @@
 #!/bin/bash
 
-echo -e "\n\n"
-
 download() {
   cd /tmp;
   rm -rf ./tgm-notifier
   git clone https://github.com/mdpanf/tgm-notifier
 }
 
-token() {
-  read -p "Input tgm-bot token: " inToken
-  read -p "Input tgm chat_id: " inChat
-  confirm
+input_data() {
+  read -p "Input tgm-bot token: " inToken;
+  read -p "Input tgm chat_id: " inChatId;
+  confirm;
 }
 
-data_input() {
-  GREEN='\033[0;32m'
-  PURPLE='\033[0;35m'
-  NC='\033[0m'
-  GR='\033\1;32m'
-
-  token() {
-    read -p "Input tgm-bot token: " inToken
-    read -p "Input tgm chat_id: " inChat
-    confirm
-  }
-
-  confirm() {
-      echo -e "Token: ${PURPLE}$inToken${NC}"
-      echo -e "Chat_id: ${PURPLE}$inChat${NC}"
-      read -e -r -p "${1:-Is it correct? [y/N]} " response
-      case "$response" in
-          [yY][eE][sS]|[yY])
-              true
-              echo "Saving...";;
-          *)
-              false
-              token;;
-      esac
-  }
-
-  echo -e "${GREEN}== Config ==${NC}"
-  token
-
-  cat > /etc/steio/tgm-notifier.conf << EOF
-Token $inToken
-Chat $inChat
-EOF
+confirm() {
+    echo -e "Token: \033[0;35m$inToken\033[0m"
+    echo -e "Chat_id: \033[0;35m$inChatId\033[0m"
+    read -e -r -p "${1:-Is it correct? [y/N]} " response
+    case "$response" in
+        [yY][eE][sS]|[yY])
+            true
+            echo "Saving...";;
+        *)
+            false
+            input_data;;
+    esac
 }
 
 install() {
@@ -55,13 +32,19 @@ install() {
 
   chmod 777 /etc/steio -R
 
-  data_input;
+  echo -e "\n\033[0;32m== Config ==\033[0m"; input_data;
+  cat > /etc/steio/tgm-notifier.conf << EOF
+Token: $inToken
+Chat: $inChat
+Icon: 🖥
+EOF
+  echo -e "\036[0;32mThe .conf file is located at \033[0;37m/etc/steio/tgm-notifier.conf\033[0m"
 
   cd /opt/steio/tgm-notifier;
   cp /tmp/tgm-notifier/startup-notifier.sh ./startup-notifier.sh
   chmod a+x ./startup-notifier.sh
 
-  cd /lib/systemd/system
+  cd /lib/systemd/system;
   cp /tmp/tgm-notifier/tgm-notifier.service ./tgm-notifier.service
 
   chmod 644 /lib/systemd/system/tgm-notifier.service
@@ -75,10 +58,12 @@ install() {
 
 main() {
   if [[ $EUID -eq 0 ]]; then
-    echo "Run script under non-root user";
+    echo -e "\n\033[0;31mRun script under non-root user\033[0m";
     echo "Abort";
     exit 1;
   fi
+
+  echo -e "\n\033[0;33mTGM-notifier by Steio\033[0m"
 
   download;
   sudo bash -c "$(declare -f install); install"
